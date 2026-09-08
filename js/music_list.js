7ba72260cf3f76b1695d952973c3f2be3e1ec74d
aimyoncaca <1322804814@qq.com>
2026-09-09 02:40:28 +0800

    Site updated: 2026-09-09 02:40:25

--- js/music_list.js
+++ js/music_list.js
@@ -4,6 +4,13 @@ var myPlaylist = [
     name: "Sandy",
     artist: "Murphy Radio",
     url: "/music/Murphy Radio - Sandy.mp3",
+    cover: "/music/cover/Sandy.png"
+  },
+  {
+    name: "No exit",
+    artist: "A Picture Of Her",
+    url: "/music/A Picture Of Her - No exit.mp3",
+    cover: "/music/cover/No exit.png"
   }
   // 以后要加新歌，直接在这里添加即可
 ];
