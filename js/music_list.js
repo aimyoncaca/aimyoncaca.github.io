92362937fdef6cc52d8a87b138b236148037b360
aimyoncaca <1322804814@qq.com>
2026-09-10 10:11:04 +0800

    Site updated: 2026-09-10 10:10:58

--- js/music_list.js
+++ js/music_list.js
@@ -11,6 +11,18 @@ var myPlaylist = [
     artist: "A Picture Of Her",
     url: "/music/A Picture Of Her - No exit.mp3",
     cover: "/music/cover/No exit.png"
+  },
+    {
+    name: "black33",
+    artist: "Novo Amor,yvette young",
+    url: "/music/Novo Amor,yvette young - black33.mp3",
+    cover: "/music/cover/black.jpg"
+  },
+  {
+    name: "monty (＾ᴥ＾) (acoustic)",
+    artist: "Novo Amor,yvette young",
+    url: "/music/Novo Amor,yvette young - monty (＾ᴥ＾) (acoustic).mp3",
+    cover: "/music/cover/monty.png"
   }
   // 以后要加新歌，直接在这里添加即可
 ];
